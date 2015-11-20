86b414f777ce3ec53d5cbd09b11eead55df8d755
dongleivip <dongleivip@gmail.com>
2015-11-20 22:20:43 +0800

    remove onclick event
    bind keyword input element with 'propertychage'
    
    rebuild ajax request into an independent function;
    
    create createList() function to display result in index page;

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,19 +1,61 @@
 $(document).ready(function() { 
+	
+	$("#keyword").bind("input propertychange", function(){
+		filterKeyword($(this).val());
+	});
+	
+//	$("#btn_search").click(function(){
+//		$.ajax({  
+//	        type:"GET",  
+//	        url:"data/bookmarks.json",  
+//	        dataType: "json",  
+//	        success: function(data){  
+//	            $.each(data,function(i,item){  
+//	                var content = "<li>" + item.title + "</li>" + "<li>" + item.created + "</li>";  
+//	                $("#list").append(content);  
+//	            })
+//	        }  
+//		})  
+//	}); 
+	
+});
 
-	$("#btn_search").click(function(){
-		$.ajax({  
+
+function filterKeyword(key){
+
+	//alert(key);
+	var reg = new RegExp(key,'gi');
+	
+	$.ajax({  
         type:"GET",  
         url:"data/bookmarks.json",  
         dataType: "json",  
-        success: function(data){  
-            $.each(data,function(i,item){  
-                var content = "<li>" + item.title + "</li>" + "<li>" + item.created + "</li>";  
-                $("#list").append(content);  
-            })  
-        }  
-    })  
-	});  
-});
+        success: function(data){
+        	//alert("success");
+        	//return ;
+        	
+        	createList(data);
+        },
+        error : function(){
+        	alert("请求数据失败");
+        }
+	})
+	
+}
+
+
+function createList(data){
+	
+	var result = data.reduce(function(str,item){
+		str += "<li class=\"list\"><div class=\"title\">" + item.title 
+			+  "</div><div class=\"createDate\">Created@" + item.created 
+			+  "</div></li>";
+		return str;
+	},"");
+	$("#list").empty();
+	$("#list").html(result);
+}
+
 	
 
     
